d550d73d8a86cf4c645ebcc5ddcf54bfad464672
Lionel Muskwe <lionel04muskwe@gmail.com>
2021-12-12 10:53:39 +0000

    Fully working application

--- CommsProject/quiz/models.py
+++ CommsProject/quiz/models.py
@@ -6,9 +6,15 @@ class Quiz(models.Model):
     # Question 1 For this quiz
     q1Question = models.CharField(max_length=100)
     q1Answer = models.CharField(max_length=100)
+    q1Explanation = models.CharField(max_length=10000, null=True, blank=True)
     # Question 2 For this quiz
     q2Question = models.CharField(max_length=100)
     q2Answer = models.CharField(max_length=100)
+    q2Explanation = models.CharField(max_length=10000, null=True, blank=True)
+     # Question 3 For this quiz
+    q3Question = models.CharField(max_length=100, null=True, blank=True)
+    q3Answer = models.CharField(max_length=100, null=True, blank=True)
+    q3Explanation = models.CharField(max_length=10000, null=True, blank=True)
 
 
     def __str__ (self):
